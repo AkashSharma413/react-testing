4b77781647109f0b7d87760a7f8f00cded1c882b
Akki <aakash.sharma413@gmail.com>
2024-08-18 19:08:42 +0530

    RTL Queries: getByRole options

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -2,18 +2,35 @@ import { render, screen } from "@testing-library/react";
 import Application from "./application";
 
 describe("Application", () => {
-    test("Renders Correctly", () => {
-        render(<Application/>);
-        let nameElement = screen.getByRole("textbox");
-        expect(nameElement).toBeInTheDocument();
+  test("Renders Correctly", () => {
+    render(<Application />);
+    let pageHeadingElement = screen.getByRole("heading", {
+      level: 1,
+    });
+    expect(pageHeadingElement).toBeInTheDocument();
 
-        let jobLocationElement = screen.getByRole("listbox");
-        expect(jobLocationElement).toBeInTheDocument();
+    let sectionHeadingElement = screen.getByRole("heading", {
+      level: 2,
+    });
+    expect(sectionHeadingElement).toBeInTheDocument();
 
-        let termElement = screen.getByRole("checkbox");
-        expect(termElement).toBeInTheDocument();
+    let nameElement = screen.getByRole("textbox", {
+      name: "Name",
+    });
+    expect(nameElement).toBeInTheDocument();
 
-        let submitButtonElement = screen.getByRole("button");
-        expect(submitButtonElement).toBeInTheDocument();
-    })
-})
+    let bioElement = screen.getByRole("textbox", {
+      name: "Bio",
+    });
+    expect(bioElement).toBeInTheDocument();
+
+    let jobLocationElement = screen.getByRole("listbox");
+    expect(jobLocationElement).toBeInTheDocument();
+
+    let termElement = screen.getByRole("checkbox");
+    expect(termElement).toBeInTheDocument();
+
+    let submitButtonElement = screen.getByRole("button");
+    expect(submitButtonElement).toBeInTheDocument();
+  });
+});
--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -2,27 +2,35 @@ import React from "react";
 
 const Application = () => {
   return (
-    <form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
-      </div>
-      <div>
-        <label htmlFor="job-location">Job Location</label>
-        <select id="job-location" role="listbox">
-          <option value="">Select Country</option>
-          <option value="us">USA</option>
-          <option value="IN">India</option>
-          <option value="CA">Canada</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="terms">
-          <input type="checkbox" id="terms" /> Accept the terms and condition
-        </label>
-      </div>
-      <button>Submit</button>
-    </form>
+    <>
+      <h1>Job Application Form</h1>
+      <h2>Section 1</h2>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" />
+        </div>
+        <div>
+          <label htmlFor="bio">Bio</label>
+          <textarea id="bio" />
+        </div>
+        <div>
+          <label htmlFor="job-location">Job Location</label>
+          <select id="job-location" role="listbox">
+            <option value="">Select Country</option>
+            <option value="us">USA</option>
+            <option value="IN">India</option>
+            <option value="CA">Canada</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="terms">
+            <input type="checkbox" id="terms" /> Accept the terms and condition
+          </label>
+        </div>
+        <button>Submit</button>
+      </form>
+    </>
   );
 };
 
