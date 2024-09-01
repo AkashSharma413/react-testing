5cf03bdc3def740ec4844b2e93f7d04cbc041158
Akki <aakash.sharma413@gmail.com>
2024-09-01 14:14:50 +0530

    getByPlaceholderText

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -24,6 +24,9 @@ describe("Application", () => {
     });
     expect(nameElement2).toBeInTheDocument();
 
+    let nameElement3 = screen.getByPlaceholderText("Fullname");
+    expect(nameElement3).toBeInTheDocument();
+
     let bioElement = screen.getByRole("textbox", {
       name: "Bio",
     });
