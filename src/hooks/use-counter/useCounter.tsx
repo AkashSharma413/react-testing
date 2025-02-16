f4fe2d63a517ee010acdcc056e5394684587a348
Akki <aakash.sharma413@gmail.com>
2025-02-17 00:15:17 +0530

    custom react hooks test cases

---
+++ src/hooks/use-counter/useCounter.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { UseCounterProp } from "./useCounter.types";
+
+export const useCounter = ({ initialCounter = 0}: UseCounterProp = {}) => {
+    const [count, setCount] = useState(initialCounter);
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+    return { count, increment, decrement };
+}
---
+++ src/hooks/use-counter/useCounter.types.tsx
@@ -0,0 +1,3 @@
+export type UseCounterProp = {
+    initialCounter?: number
+}
