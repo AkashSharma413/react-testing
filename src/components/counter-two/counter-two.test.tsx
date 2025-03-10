7f778ced02099b50f70bf233826574731df92918
Akki <aakash.sharma413@gmail.com>
2025-03-10 18:57:18 +0530

    mocking functions

---
+++ src/components/counter-two/counter-two.test.tsx
@@ -0,0 +1,25 @@
+import {render, screen} from "@testing-library/react";
+import { CounterTwo } from "./counter-two";
+import user from "@testing-library/user-event";
+
+describe("CounterTwo", () => {
+    test("Renders Correctly", () => {
+        render(<CounterTwo count={0} />);
+        const textElement = screen.getByText("Counter Two");
+        expect(textElement).toBeInTheDocument();
+    });
+    
+    test("Handlers are called", async () => {
+        user.setup();
+        const incrementHandler = jest.fn();
+        const decrementHandler = jest.fn();
+
+        render(<CounterTwo count={0} handleIncrement={incrementHandler} handleDecrement={decrementHandler}/>);
+        const incrementButton = screen.getByRole("button", {name: "Increment"});
+        const decrementButton = screen.getByRole("button", {name: "Decrement"});
+        await user.click(incrementButton);
+        await user.click(decrementButton);
+        expect(incrementHandler).toHaveBeenCalledTimes(1);
+        expect(decrementHandler).toHaveBeenCalledTimes(1);
+    })
+})
---
+++ src/components/counter-two/counter-two.tsx
@@ -0,0 +1,20 @@
+import { CounterTwoProps } from './counter-two.types';
+
+export const CounterTwo = (props: CounterTwoProps) => {
+    return(
+        <>
+            <h1>Counter Two</h1>
+            <p>{props.count}</p>
+            {
+                props.handleIncrement && (
+                    <button onClick={props.handleIncrement}>Increment</button>
+                )
+            }
+            {
+                props.handleDecrement && (
+                    <button onClick={props.handleDecrement}>Decrement</button>
+                )
+            }
+        </>
+    );
+}
